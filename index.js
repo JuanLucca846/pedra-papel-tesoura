8e84ca5a6cc42c0edc3a3d0b7edd88272777f2de
JuanLucca846 <71161428+JuanLucca846@users.noreply.github.com>
2022-05-20 21:27:23 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,64 @@
+const computerChoiceDisplay = document.getElementById('computer-choice')
+const userChoiceDisplay = document.getElementById('user-choice')
+const resultDisplay = document.getElementById('result')
+const possibleChoices = document.querySelectorAll('button')
+let userChoice
+let computerChoice
+let result
+
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+	userChoice = e.target.id
+	userChoiceDisplay.innerHTML = userChoice
+	generateComputerChoice()
+	getResult()
+}))
+
+function generateComputerChoice() {
+	const randomNumber = Math.floor(Math.random() * 3) + 1
+
+	if (randomNumber === 1) {
+		computerChoice = 'pedra'
+	}
+
+	if (randomNumber === 2) {
+		computerChoice = 'tesoura'
+	}
+
+	if (randomNumber === 1) {
+		computerChoice = 'papel'
+	}
+
+	computerChoiceDisplay.innerHTML = computerChoice
+}
+
+function getResult(){
+	if (computerChoice === userChoice) {
+		result = 'Empate!'
+	}
+
+	if (computerChoice === 'pedra' && userChoice === 'papel') {
+		result = 'Você venceu!'
+	}
+
+	if (computerChoice === 'pedra' && userChoice === 'tesoura') {
+		result = 'Você perdeu!'
+	}
+
+	if (computerChoice === 'papel' && userChoice === 'tesoura') {
+		result = 'Você venceu!'
+	}
+
+	if (computerChoice === 'papel' && userChoice === 'pedra') {
+		result = 'Você perdeu!'
+	}
+
+	if (computerChoice === 'tesoura' && userChoice === 'pedra') {
+		result = 'Você venceu!'
+	}
+
+	if (computerChoice === 'tesoura' && userChoice === 'papel') {
+		result = 'Você perdeu!'
+	}
+
+	resultDisplay.innerHTML = result
+}
